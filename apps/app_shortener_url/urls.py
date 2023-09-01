bc2c8f514b6676f1b7ea7552ec9c961a1577c1e8
chimaek <pipiru100@gmail.com>
2023-09-01 16:41:06 +0900

    :sparkles: Test Code 추가, admin url 제거

--- apps/app_shortener_url/urls.py
+++ apps/app_shortener_url/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from apps.app_shortener_url import views
 
 urlpatterns = [
-    # path("home/", views.home, name="home"),
+    path("home/", views.home, name="home"),
     path("shortener/", views.convert_url, name="convert_url"),
     path("<str:short_url>", views.redirect_url, name="redirect_url"),
 ]
