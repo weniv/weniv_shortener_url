0dc3919bc7998d1377a6283ffaa6625b25147181
chimaek <pipiru100@gmail.com>
2023-09-04 17:45:48 +0900

    :sparkles: Dockerfile 추가 및 환경 변수 사용을 위한 environ 모듈 추가

--- shortener_url/config/settings.py
+++ shortener_url/config/settings.py
@@ -12,15 +12,20 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
+import environ
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-APPS_DIR = os.path.join(BASE_DIR, 'apps')
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+APPS_DIR = os.path.join(BASE_DIR, "../apps")
+TEMPLATES_DIR = os.path.join(BASE_DIR, "../templates")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+env = environ.Env()
+print(BASE_DIR)
+environ.Env.read_env(env_file=os.path.join(BASE_DIR, "../.env"))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-yi9m=csalkyg640t-%)4bya3ta*sqiwrcq$c*@&dy4x=2m$y*z"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,7 +36,6 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     "django_extensions",
-    "rest_framework",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -45,7 +49,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    # "django.middleware.csrf.CsrfViewMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
@@ -77,7 +81,7 @@ WSGI_APPLICATION = "shortener_url.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3", #
     }
 }
 
@@ -110,15 +114,16 @@ USE_I18N = True
 
 USE_TZ = False
 
+DEFAULT_CHARSET = "utf-8"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "../static") #
+# ]
+STATIC_ROOT = os.path.join(BASE_DIR, "../static") #
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
