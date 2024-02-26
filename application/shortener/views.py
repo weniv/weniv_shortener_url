06cb13cf9cb5e7a334ab1b4dd2382fe300c66787
seungju.kim <pipiru100@gmail.com>
2024-02-26 15:56:29 +0900

    url_shortener 리팩토링

--- application/shortener/views.py
+++ application/shortener/views.py
@@ -5,32 +5,45 @@ import hashlib
 import base64
 
 
-# Create your views here.
-
-
 def index(request):
     if request.method == 'POST':
         original_url = request.POST.get('original_url')
-
-        shorten_url = generate_shorten_url(original_url)
-        if ShortenURL.objects.filter(shorten_url=shorten_url).exists():
-            return render(request, 'shortener/index.html', {'shorten_url': shorten_url})
-        shorten_url_obj = ShortenURL(original_url=original_url, shorten_url=shorten_url)
-        shorten_url_obj.save()
-        cache.set(shorten_url, original_url)
-        return render(request, 'shortener/index.html', {'shorten_url': shorten_url})
+        context = manage_url(original_url)
+        return render(request, 'shortener/index.html', context)
     return render(request, 'shortener/index.html')
 
 
-def generate_shorten_url(original_url: str) -> str:
-    hash_data = hashlib.sha256(original_url.encode('utf-8')).digest()
-    shorten_url = base64.urlsafe_b64encode(hash_data).decode('utf-8')[:6]
+def manage_url(original_url):
+    context = {'original_url': original_url}
+    shorten_url = generate_or_fetch_shorten_url(original_url)
+    context.update(shorten_url=shorten_url, created_at=ShortenURL.objects.get(shorten_url=shorten_url).created_at)
+    return context
+
+
+def generate_or_fetch_shorten_url(original_url):
+    shorten_url = generate_shorten_url(original_url)
+    if not ShortenURL.objects.filter(shorten_url=shorten_url).exists():
+        ShortenURL(original_url=original_url, shorten_url=shorten_url).save()
+        cache.set(shorten_url, original_url)
     return shorten_url
 
 
+def generate_shorten_url(original_url: str) -> str:
+    for _ in range(10):  # Try up to 3 times to avoid hash collisions.
+        hash_data = hashlib.sha256(original_url.encode('utf-8')).digest()
+        shorten_url = base64.urlsafe_b64encode(hash_data).decode('utf-8')[:6]
+        if not ShortenURL.objects.filter(shorten_url=shorten_url).exists():
+            return shorten_url
+        original_url += '0'  # Adjust the URL slightly to try and avoid collisions.
+    raise ValueError("Unable to generate a unique shorten URL")
+
+
 def redirect_original_url(request, shorten_url):
-    original_url = cache.get(shorten_url)
-    if original_url is None:
-        original_url = ShortenURL.objects.get(shorten_url=shorten_url).original_url
-        cache.set(shorten_url, original_url)
+    original_url = cache.get(shorten_url) or fetch_and_cache_original_url(shorten_url)
     return redirect(original_url)
+
+
+def fetch_and_cache_original_url(shorten_url):
+    original_url = ShortenURL.objects.get(shorten_url=shorten_url).original_url
+    cache.set(shorten_url, original_url)
+    return original_url
