a2db7d8c0f7e1f64faf6b2a0059501b56b1f8225
seungju.kim <pipiru100@gmail.com>
2024-09-23 17:05:55 +0900

    로직 변경(url 추출 api추가 및 유효성 검증 수정)

--- application/shortener/urls.py
+++ application/shortener/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'shortener'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<str:shorten_url_code>', views.redirect_original_url, name='redirect_original_url'),
+    path('<str:shorten_url_code>/', views.redirect_original_url, name='redirect_original_url'),
     # 직원 전용 url 줄이기 기능
     path('staff/shorten_url', views.staff_index, name='staff_index'),
     path('api/generate', views.api_generate_shorten_url, name='generate_shorten_url'),
