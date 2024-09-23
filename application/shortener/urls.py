10003d94aa5261d3c0582b2ccd7796a5b1a22648
seungju.kim <pipiru100@gmail.com>
2024-09-23 09:24:56 +0900

    직원 전용 url_shoten 추가

--- application/shortener/urls.py
+++ application/shortener/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<str:shorten_url_code>/', views.redirect_original_url, name='redirect_original_url'),
     # 직원 전용 url 줄이기 기능
-    path('staff/', views.staff_index, name='staff_index'),
+    path('staff/shorten_url', views.staff_index, name='staff_index'),
 
 ]
